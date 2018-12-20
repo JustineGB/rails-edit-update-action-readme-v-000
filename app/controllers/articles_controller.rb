0c5142bcdc85f18b9640309b0b7c7cf568b22bd2
Justine B <justinegayton@gmail.com>
2018-12-20 19:59:20 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(title: params[:title], description: params[:description])
     @article.save
-    raise params.inspect
+    #raise params.inspect
     redirect_to article_path(@article)
   end
   
